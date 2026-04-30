e93c410e8d453bf4f5180efb1b31c81a29320743
aleasweb <aleas.web@gmail.com>
2026-04-30 18:10:51 +0300

    re-design

--- src/DesktopApp/Views/ChatView.axaml.cs
+++ src/DesktopApp/Views/ChatView.axaml.cs
@@ -1,3 +1,4 @@
+using Avalonia.Input;
 using DesktopApp.ViewModels;
 
 namespace DesktopApp.Views;
@@ -7,5 +8,26 @@ public partial class ChatView : Avalonia.Controls.UserControl
     public ChatView()
     {
         InitializeComponent();
+    }
+
+    private void OnTextBoxKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter || e.Key == Key.Return)
+        {
+            if (e.KeyModifiers.HasFlag(KeyModifiers.Shift))
+            {
+                // Позволяем TextBox обработать Shift+Enter (перенос строки)
+                return;
+            }
+            
+            // Предотвращаем стандартное поведение (перенос строки)
+            e.Handled = true;
+            
+            // Выполняем команду отправки сообщения
+            if (DataContext is ChatViewModel vm && vm.SendMessageCommand.CanExecute(null))
+            {
+                vm.SendMessageCommand.Execute(null);
+            }
+        }
     }
 }
